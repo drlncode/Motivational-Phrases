d4098105f5e072355ecb90f58ef6e68c0577ee62
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-07-08 10:18:17 -0400

    Updating backEnd.

--- app/App.js
+++ app/App.js
@@ -1,23 +1,24 @@
 import express from 'express';
-import { readFile } from 'node:fs';
+import { readFile } from 'node:fs/promises';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+    app.use(express.static('public'));
+});
 
-app.get('/phrases', (req, res) => {
-    readFile(`${__dirname}/data/phrases.json`, 'utf-8', (err, data) => {
-        if (err) {
-            res.status(500).send('500 - Internal Server Error');
-            return;
-        }
-
+app.get('/phrases', async (req, res) => {
+    try {
+        const data = await readFile(`${__dirname}/data/phrases.json`, 'utf-8');
         res.setHeader('Content-Type', 'application/json');
         res.send(data);
-    });
+    } catch (error) {
+        res.status(500).send('500 - Internal Server Error');
+        console.log(error);
+    }
 });
 
 app.use((req, res) => {
