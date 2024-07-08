ae2f8cc315e98c1be2f9e5fd1ace6f5ebb23ebde
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-07-08 11:08:01 -0400

    Fixing error: dont find static files.

--- app/App.js
+++ app/App.js
@@ -6,8 +6,6 @@ import { fileURLToPath } from 'node:url';
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
     const indexFile = path.join(__dirname, '../public', 'index.html');
     res.sendFile(indexFile);
@@ -22,6 +20,25 @@ app.get('/phrases', async (req, res) => {
         res.status(500).send('500 - Internal Server Error');
         console.log(error);
     }
+});
+
+// app.use(express.static()) dont work in production, idk why.
+app.get('/public/js/:file', (req, res, next) => {
+    const { file } = req.params;
+    const filePath = path.join(__dirname, '../public/js', file);
+    
+    res.sendFile(filePath, (err) => {
+        if (err) res.status(404).send('404 Not Found.');
+    });
+}); 
+
+app.get('/public/css/:file', (req, res) => {
+    const { file } = req.params;
+    const filePath = path.join(__dirname, '../public/css', file);
+    
+    res.sendFile(filePath, (err) => {
+        if (err) res.status(404).send('404 Not Found.');
+    });
 });
 
 app.use((req, res) => {
