e750559539108e5ac6e1e186ace3b6838121a4e9
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-07-09 12:59:49 -0400

    Inncesary code deleted.

--- public/js/index.js
+++ public/js/index.js
@@ -113,7 +113,6 @@
     
         results.push(random);
         actualPhrase = random;
-        console.log(actualPhrase);
 
         // Old linear operation:
         //
@@ -127,7 +126,6 @@
     }
 
     function deviceButton(e) {
-        const pcPauseButtonContent = topauseButton.innerHTML;
         const mobilePauseButtonContent = `
             <p> <svg xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"
                 fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-player-pause">
