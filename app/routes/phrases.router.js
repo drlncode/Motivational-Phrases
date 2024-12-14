6b03d501bfe67f62c2167211b67534f30276bb22
Darlin Daniel Arias M. <darlin.code@gmail.com>
2024-12-14 12:06:13 -0400

    rework begins.

--- app/routes/phrases.router.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { PhrasesController } from '../controllers/phrases.controller.js';
-
-export const phrasesRouter = Router();
-
-phrasesRouter.get('/', PhrasesController.getPhrases);
--- index.js
+++
@@ -1,6 +0,0 @@
-import app from './app/App.js';
-const PORT = process.env.PORT ?? 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
